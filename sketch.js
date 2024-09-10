730fdae4c34c1354f80f15355747dfc462173df4
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2024-09-09 22:52:42 -0400

    multiple boxes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const SCREEN_WIDTH = window.innerWidth;
-const SCREEN_HEIGHT = window.innerHeight -100;
+const SCREEN_HEIGHT = window.innerHeight - 100;
 
 let force_x = 0;
 let force_y = 980; // Gravity (increased for more visible effect)
@@ -20,6 +20,7 @@ class Point {
     this.pinned = pinned;
     this.radius = 10;
   }
+
   update(dt) {
     if (!this.pinned) {
       let vel_x = (this.x - this.old_x) * 0.99; // Add some damping
@@ -35,6 +36,7 @@ class Point {
       this.y += vel_y + acc_y * dt * dt;
     }
   }
+
   constrain() {
     let vel_x = (this.x - this.old_x);
     let vel_y = (this.y - this.old_y);
@@ -53,11 +55,13 @@ class Point {
       this.old_y = this.y + vel_y * 0.9;
     }
   }
+
   render() {
     noStroke();
     fill("white");
     circle(this.x, this.y, this.radius * 2);
   }
+
   isMouseOver() {
     return distance(this, {x: mouseX, y: mouseY}) < this.radius;
   }
@@ -68,7 +72,9 @@ class Stick {
     this.p0 = p0;
     this.p1 = p1;
     this.restLength = restLength;
+    this.diagonal = diagonal;
   }
+
   update(dt) {
     let dx = this.p1.x - this.p0.x;
     let dy = this.p1.y - this.p0.y;
@@ -89,122 +95,186 @@ class Stick {
       this.p1.y += offset_y;
     }
   }
+
   render() {
     stroke('gray');
     line(this.p0.x, this.p0.y, this.p1.x, this.p1.y);
   }
 }
 
-let points = [
-  new Point(250, 250, 1.0, false),  // A
-  new Point(300, 250, 1.0, false),  // B
-  new Point(300, 300, 1.0, false),  // C
-  new Point(250, 300, 1.0, false),  // D
-];
+class Box {
+  constructor(centerX, centerY, width, height) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.width = width;
+    this.height = height;
+    this.points = this.createPoints();
+    this.sticks = this.createSticks();
+    this.draggingPoint = null;
+    this.initialMouseX = 0;
+  }
 
-let sticks = [
-  new Stick(points[0], points[1], distance(points[0], points[1])),  //  A-----B
-  new Stick(points[1], points[2], distance(points[1], points[2])),  //  | \   |
-  new Stick(points[2], points[3], distance(points[2], points[3])),  //  |  \  |
-  new Stick(points[3], points[0], distance(points[3], points[0])),  //  |   \ |
-  new Stick(points[0], points[2], distance(points[0], points[2]), true),  //  D-----C
-];
+  createPoints() {
+    return [
+      new Point(this.centerX - this.width/2, this.centerY - this.height/2, 1.0, false),  // A
+      new Point(this.centerX + this.width/2, this.centerY - this.height/2, 1.0, false),  // B
+      new Point(this.centerX + this.width/2, this.centerY + this.height/2, 1.0, false),  // C
+      new Point(this.centerX - this.width/2, this.centerY + this.height/2, 1.0, false),  // D
+    ];
+  }
 
-let draggingPoint = null;
-let initialMouseX = 0;
+  createSticks() {
+    return [
+      new Stick(this.points[0], this.points[1], distance(this.points[0], this.points[1])),  //  A-----B
+      new Stick(this.points[1], this.points[2], distance(this.points[1], this.points[2])),  //  | \   |
+      new Stick(this.points[2], this.points[3], distance(this.points[2], this.points[3])),  //  |  \  |
+      new Stick(this.points[3], this.points[0], distance(this.points[3], this.points[0])),  //  |   \ |
+      new Stick(this.points[0], this.points[2], distance(this.points[0], this.points[2]), true),  //  D-----C
+    ];
+  }
+
+  update(dt) {
+    for (let point of this.points) {
+      point.update(dt);
+    }
+
+    for (let i = 0; i < 10; i++) {
+      for (let stick of this.sticks) {
+        stick.update(dt);
+      }
+      this.maintainRightAngles();
+    }
+    
+    for (let point of this.points) {
+      point.constrain();
+    }
+  }
+
+  render() {
+    for (let stick of this.sticks) {
+      stick.render();
+    }
+    for (let point of this.points) {
+      point.render();
+    }
+  }
+
+  maintainRightAngles() {
+    let avgWidth = (this.sticks[0].restLength + this.sticks[2].restLength) / 2;
+    let avgHeight = (this.sticks[1].restLength + this.sticks[3].restLength) / 2;
+
+    let centerX = (this.points[0].x + this.points[1].x + this.points[2].x + this.points[3].x) / 4;
+    let centerY = (this.points[0].y + this.points[1].y + this.points[2].y + this.points[3].y) / 4;
+
+    this.points[0].x = centerX - avgWidth / 2;
+    this.points[0].y = centerY - avgHeight / 2;
+
+    this.points[1].x = centerX + avgWidth / 2;
+    this.points[1].y = centerY - avgHeight / 2;
+
+    this.points[2].x = centerX + avgWidth / 2;
+    this.points[2].y = centerY + avgHeight / 2;
+
+    this.points[3].x = centerX - avgWidth / 2;
+    this.points[3].y = centerY + avgHeight / 2;
+
+    this.sticks[4].restLength = distance(this.points[0], this.points[2]);
+  }
+
+  handleMousePressed(mouseX, mouseY) {
+    for (let point of this.points) {
+      if (point.isMouseOver()) {
+        this.draggingPoint = point;
+        this.initialMouseX = mouseX;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  handleMouseDragged(mouseX) {
+    if (this.draggingPoint) {
+      let dx = mouseX - this.initialMouseX;
+      
+      // Determine which side the dragging point is on
+      let isLeftSide = (this.draggingPoint === this.points[0] || this.draggingPoint === this.points[3]);
+      
+      // Move the points on the same side
+      if (isLeftSide) {
+        this.points[0].x += dx;
+        this.points[3].x += dx;
+      } else {
+        this.points[1].x += dx;
+        this.points[2].x += dx;
+      }
+      
+      // Update old positions to prevent sudden velocity changes
+      for (let point of this.points) {
+        point.old_x = point.x;
+      }
+
+      // Update stick rest lengths for horizontal sticks only
+      this.sticks[0].restLength = distance(this.points[0], this.points[1]);
+      this.sticks[2].restLength = distance(this.points[2], this.points[3]);
+      
+      this.initialMouseX = mouseX;
+      return true;
+    }
+    return false;
+  }
+
+  handleMouseReleased() {
+    if (this.draggingPoint) {
+      this.draggingPoint = null;
+      return true;
+    }
+    return false;
+  }
+}
+
+let boxes = [];
 
 function setup() {
   canvas = createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT+100);
+  // Create multiple boxes
+  boxes.push(new Box(250, SCREEN_HEIGHT-30, 50, 50));
+  boxes.push(new Box(400, SCREEN_HEIGHT-30, 50, 50));
+  boxes.push(new Box(550, SCREEN_HEIGHT-30, 50, 50));
 }
 
 function draw() {  
   let dt = deltaTime / 1000;
   
-  for (let point of points) {
-    point.update(dt);
-  }
-
-  for (let i = 0; i < 10; i++) {
-    for (let stick of sticks) {
-      stick.update(dt);
-    }
-    maintainRightAngles();
-  }
-  
-  for (let point of points) {
-    point.constrain();
+  for (let box of boxes) {
+    box.update(dt);
   }
   
   background('black');
-  for (let stick of sticks) {
-    stick.render();
+  for (let box of boxes) {
+    box.render();
   }
-  for (let point of points) {
-    point.render();
-  }
-}
-
-function maintainRightAngles() {
-  let avgWidth = (sticks[0].restLength + sticks[2].restLength) / 2;
-  let avgHeight = (sticks[1].restLength + sticks[3].restLength) / 2;
-
-  let centerX = (points[0].x + points[1].x + points[2].x + points[3].x) / 4;
-  let centerY = (points[0].y + points[1].y + points[2].y + points[3].y) / 4;
-
-  points[0].x = centerX - avgWidth / 2;
-  points[0].y = centerY - avgHeight / 2;
-
-  points[1].x = centerX + avgWidth / 2;
-  points[1].y = centerY - avgHeight / 2;
-
-  points[2].x = centerX + avgWidth / 2;
-  points[2].y = centerY + avgHeight / 2;
-
-  points[3].x = centerX - avgWidth / 2;
-  points[3].y = centerY + avgHeight / 2;
-
-  sticks[4].restLength = distance(points[0], points[2]);
 }
 
 function mousePressed() {
-  for (let point of points) {
-    if (point.isMouseOver()) {
-      draggingPoint = point;
-      initialMouseX = mouseX;
+  for (let box of boxes) {
+    if (box.handleMousePressed(mouseX, mouseY)) {
       break;
     }
   }
 }
 
 function mouseDragged() {
-  if (draggingPoint) {
-    let dx = mouseX - initialMouseX;
-    
-    // Determine which side the dragging point is on
-    let isLeftSide = (draggingPoint === points[0] || draggingPoint === points[3]);
-    
-    // Move the points on the same side
-    if (isLeftSide) {
-      points[0].x += dx;
-      points[3].x += dx;
-    } else {
-      points[1].x += dx;
-      points[2].x += dx;
+  for (let box of boxes) {
+    if (box.handleMouseDragged(mouseX)) {
+      break;
     }
-    
-    // Update old positions to prevent sudden velocity changes
-    for (let point of points) {
-      point.old_x = point.x;
-    }
-
-    // Update stick rest lengths for horizontal sticks only
-    sticks[0].restLength = distance(points[0], points[1]);
-    sticks[2].restLength = distance(points[2], points[3]);
-    
-    initialMouseX = mouseX;
   }
 }
 
 function mouseReleased() {
-  draggingPoint = null;
+  for (let box of boxes) {
+    if (box.handleMouseReleased()) {
+      break;
+    }
+  }
 }
